f1d55a7b46fda3668de1c1f004d2d9a19289ca3e
Alyssa Jennifer Swift <aswift@node28.boisestate.edu>
2014-09-12 13:31:26 -0600

    removed unit tests evaluations folder, created unitTest folder, started outline of unit tests

---
+++ unitTests/spec/NineManMorrisSpec.js
@@ -0,0 +1,30 @@
+
+describe("Game is created", function() {
+    it("creates a stage", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("creates a layer for the game board", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("creates a game board", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("creates 3 boxes and draws them on the game board", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("adds game board to game board layer", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("creates 9 red and 9 white game pieces and adds to game board layer", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("adds game board layer to the stage", function() {
+        expect(true).toEqual(true);
+    });
+});
