bbe9dbcc9f73473d3a1248556a2430838dae246e
NerdcoreSteve <dontworshipsteve@gmail.com>
2014-10-20 16:20:20 -0600

    added some dummy tests

--- spec/NineManMorrisSpec2.js
+++ spec/NineManMorrisSpec2.js
@@ -1,38 +1,26 @@
-describe("Game is created", function() {
-    it("creates a stage", function() {
-       var ExpectedStage = new Kinetic.Stage({
-        container: 'container',
-        width: 500,
-        height: 600
-    });
-        var ActualStage = gameFactory.createGame();
-        expect(ActualStage.attrs).toEqual(ExpectedStage.attrs);
+describe("Decremantar", function() {
+    it("Keeps track of how many pieces are on the board", function() {
     });
 
+    it("returns true if the number of pieces for a player is less than three.", function() {
+    });
+});
 
-    it("creates a game board", function() {
-        var Expected_game_board = new Kinetic.Group({
-            x: 50,
-            y: 80
-        });
-        var Actual_game_board = gameFactory.createGameboard();
-        //Kinetic objects are a bit complex, if you just grab attrs, it contains the attributes we actually set, such as x and y here
-        expect(Actual_game_board.attrs).toEqual(Expected_game_board.attrs);
+describe("GameBoard", function() {
+    it("Checks if a player has 3 or less pieces", function() {
     });
 
-    it("creates 3 boxes and draws them on the game board", function() {
-        expect(true).toEqual(true);
+    it("Checks to see if two pieces are neighbors", function() {
     });
 
-    it("adds game board to game board layer", function() {
-        expect(true).toEqual(true);
+    it("Checks to see if a piece is in a mill", function() {
+    });
+});
+
+describe("GamePiece", function() {
+    it("Can set itself draggable", function() {
     });
 
-    it("creates 9 red and 9 white game pieces and adds to game board layer", function() {
-        expect(true).toEqual(true);
-    });
-
-    it("adds game board layer to the stage", function() {
-        expect(true).toEqual(true);
+    it("Knows what space it's in", function() {
     });
 });
