76bc5cadd8a5d9feee2375cdcb4a337dc84fbf3b
DustinCalkins <dustin.j.calkins@gmail.com>
2014-10-20 23:26:26 -0600

    Added decremantar tests from other spec

--- spec/NineManMorrisSpec.js
+++ spec/NineManMorrisSpec.js
@@ -223,3 +223,26 @@ describe("Transitions from 1 to 2", function() {
     
       
 });
+
+
+describe("Decremantar", function() {
+    it("Keeps track of how many pieces are on the board", function() {
+        decremantar = new Decremantar();
+        decremantar.decrement("white");
+        decremantar.decrement("red");
+        decremantar.decrement("white");
+        decremantar.decrement("red");
+        expect(decremantar.red).toEqual(7);
+        expect(decremantar.white).toEqual(7);
+    });
+
+    it("returns true if the number of pieces for a player is less than three.", function() {
+        decremantar = new Decremantar();
+        for(i = 0; i < 6; i++) {
+            decremantar.decrement("white");
+            decremantar.decrement("red");
+        }
+        expect(decremantar.hasThreeOrLess("red")).toEqual(true);
+        expect(decremantar.hasThreeOrLess("white")).toEqual(true);
+    });
+});
